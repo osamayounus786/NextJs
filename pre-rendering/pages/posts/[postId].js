c230cb9119d74cdefe156176ccc4114fc7cbb605
Osama Younus <osamayounus541@gmail.com>
2023-02-09 15:15:50 +0500

    error fixed

--- pre-rendering/pages/posts/[postId].js
+++ pre-rendering/pages/posts/[postId].js
@@ -10,10 +10,26 @@ function Post({posts}){
 }
 export default Post;
 
+export async function getStaticPaths(){
+
+    return{
+        paths: [{
+            params: {postId: '1'},
+        },
+        {
+            params: {postId: '2'},
+        },
+        {
+            params: {postId: '3'},
+        },],
+        fallback: false,
+    }
+}
+
 export async function getStaticProps(context){
     const {params} = context;
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
-    const data = response.json();
+    const data = await response.json();
 
     return{
         props:{
--- pre-rendering/pages/posts/index.js
+++ pre-rendering/pages/posts/index.js
@@ -20,6 +20,8 @@ export default function PostsList({posts}){
         )
 }
 
+
+
 export async function getStaticProps(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts')
     const data = await response.json();
