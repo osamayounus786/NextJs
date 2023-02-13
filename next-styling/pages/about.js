672705cb47239e78570689310d5e6439738e8509
Osama Younus <osamayounus541@gmail.com>
2023-02-13 18:57:24 +0500

    style css CLR

---
+++ next-styling/pages/about.js
@@ -0,0 +1,9 @@
+import styles from '../styles/About.module.css'
+export default function About(){
+    return(
+        <>
+        <h2>About Page</h2>
+        <h3 className={styles.highlight}>Hello component scope css</h3>
+        </>
+    )
+}
---
+++ next-styling/pages/contact.js
@@ -0,0 +1,9 @@
+import styles from '../styles/Contact.module.css'
+
+export default function Contact(){
+    return(
+        <>
+        <h2>Hello Orange</h2>
+        <h3 className={styles.highlight}>Hello component scope css</h3>        </>
+    )
+}
