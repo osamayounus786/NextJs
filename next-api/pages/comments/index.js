9b08aaad0639a9263ce41e11f6b93a3fdcbc991f
Osama Younus <osamayounus541@gmail.com>
2023-02-13 13:52:17 +0500

    post req

--- next-api/pages/comments/index.js
+++ next-api/pages/comments/index.js
@@ -1,28 +1,52 @@
-import { useState } from "react"
+import { useState } from 'react'
+function CommentsPage() {
+  const [comments, setComments] = useState([]) // get request method
+  const [comment, setComment] = useState('') // post request method
+  
+  const fetchComments = async () => { // get request method
+    const response = await fetch('/api/comments')
+    const data = await response.json()
+    setComments(data)
+  }
 
-export default function CommentsPages(){
-    const [comments, setComments] = useState([])
-    const fetchComments = async () =>{
-            const response = await fetch('/api/comments')
-            const data = await response.json()
-            setComments(data)
-    }
-    return(
-        
-        <>
-        <button onClick={fetchComments}>
-            Load Comments
-        </button>
-        {
-            comments.map((comment)=>{
-                    return (
-                        <div key={comment.id}>
-                               {comment.id} 
-                               {comment.text} 
-                        </div>
-                    )
-            })
-        }
-        </>
-    )
+  const submitComment = async () => { // post request method
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({ comment }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await response.json()
+    console.log(data)
+  }
+
+  
+  return (
+    <>
+     {/* // post request method */}
+      <div>
+        <input
+          type='text'
+          value={comment}
+          onChange={e => setComment(e.target.value)}
+        />
+        <button onClick={submitComment}>Submit comment</button>
+      </div>
+      <hr />
+      {/* // get request method */}
+      <button onClick={fetchComments}>Load comments</button> 
+
+      {comments.map(comment => {
+        return (
+          <div key={comment.id}>
+            {comment.id}. {comment.text}
+            
+          </div>
+        )
+      })}
+    </>
+  )
 }
+
+export default CommentsPage
