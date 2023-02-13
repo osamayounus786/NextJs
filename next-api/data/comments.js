288caa8b5f9bee88781a63fbb1b8690b445772ad
Osama Younus <osamayounus541@gmail.com>
2023-02-13 12:30:09 +0500

    get request

---
+++ next-api/data/comments.js
@@ -0,0 +1,15 @@
+export const comments= [
+    {
+        id: 1,
+        text: 'This is first comment'
+    },
+    {
+        id: 2,
+        text: 'This is second comment'
+    },
+    {
+        id: 3,
+        text: 'This is third comment'
+    },
+    
+]
