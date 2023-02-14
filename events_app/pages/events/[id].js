bf0977abda6b3d72dd3bd18f5a4fda77caf3ca50
Osama Younus <osamayounus541@gmail.com>
2023-02-14 18:00:26 +0500

    dynamic event

---
+++ events_app/pages/events/[id].js
@@ -0,0 +1,7 @@
+const Page = () =>{
+    return(
+        <h1> Dynamic Event Page</h1>
+    )
+}
+
+export default Page;
---
+++ events_app/pages/events/index.js
@@ -0,0 +1,26 @@
+const Page = () =>{
+    return(
+            <div>
+            <h1>Events Page</h1>
+            <div>
+            <a href=''>
+                    <img/>
+                    <h2>Events in London</h2>
+                    
+                </a>
+                <a href=''>
+                    <img/>
+                    <h2>Events in Sanfracisco</h2>
+                    
+                </a>
+                <a href=''>
+                    <img/>
+                    <h2>Events in Barceclona</h2>
+                    
+                </a>
+            </div>
+            </div>
+    )
+}
+
+export default Page;
