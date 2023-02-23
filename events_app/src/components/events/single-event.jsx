7c0d0017e38bf7249f38c2c71708a9cf22394111
Osama Younus <osamayounus541@gmail.com>
2023-02-23 14:25:22 +0500

    finished

--- events_app/src/components/events/single-event.jsx
+++ events_app/src/components/events/single-event.jsx
@@ -1,26 +1,46 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import React, { useRef, useState } from 'react';
 
- 
- const SingleEvent =  ({data}) =>{
-    return(
-        <div>
-        <h1>{
-           data.title
-       }</h1>
-       <Image src={
-               data.image
-           }
-           width={1000}
-           height={500}
-           alt={
-               data.title
-           }
-           priority
-           />
-      
-       <p>{
-           data.description
-       }</p>
+const SingleEvent = ({ data }) => {
+  const inputEmail = useRef();
+  const router = useRouter();
+  const [message, setMessage] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = inputEmail.current.value;
+    const eventId = router?.query.id;
+
+    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!emailValue.match(validRegex)) {
+      setMessage('Please introduce a correct email address');
+    }
+
+    try {
+      const response = await fetch('/api/email-registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email: emailValue, eventId }),
+      });
+
+      if (!response.ok) throw new Error(`Error: ${response.status}`);
+      const data = await response.json();
+      setMessage(data.message);
+      inputEmail.current.value = '';
+    } catch (e) {
+      console.log('ERROR', e);
+    }
+  };
+
+  return (
+    <div className="event_single_page">
+      <h1> {data.title} </h1>
+      <Image src={data.image} width={1000} height={500} alt={data.title} />
+      <p> {data.description} </p>
       <form onSubmit={onSubmit} className="email_registration">
         <label> Get Registered for this event!</label>
         <input
@@ -31,8 +51,9 @@ import Image from "next/image";
         />
         <button type="submit"> Submit</button>
       </form>
-   </div>
-    )
- }
+      <p>{message}</p>
+    </div>
+  );
+};
 
- export default SingleEvent;
+export default SingleEvent;
